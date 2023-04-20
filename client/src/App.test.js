35dac71a09ec901495e3bc573e56bf8b67ab04fe
MattFossett324 <60157991+MattFossett324@users.noreply.github.com>
2023-04-20 16:02:42 -0500

    Update App.test.js
    added a couple tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+ test("renders the title correctly", () => {
+    const { getByText } = render(<App />);
+    const title = getByText(/Memory Master/i);
+    expect(title).toBeInTheDocument();
+ });
+
+ test("shows the player stats when clicking on the player stats button", async () => {
+    const { getByText } = render(<App />);
+    const playerStatsButton = getByText(/Player Stats./i);
+    fireEvent.click(playerStatsButton);
+    await waitFor(() => expect(getByText(/Highscore:/i)).toBeInTheDocument());
+ });
+
+
